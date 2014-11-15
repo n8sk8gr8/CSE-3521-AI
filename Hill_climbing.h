9b69b058dbe61b1f30d2d479f1459501acd08fa7
n8sk8gr8 <portville99@gmail.com>
2014-11-14 20:19:30 -0500

    Initial Commit
    Parse input file into state variables of Board class.

---
+++ Hill_climbing.h
@@ -0,0 +1,40 @@
+/*
+ * Hill_climbing.h
+ *
+ *  Created on: Nov 14, 2014
+ *      Author: n8
+ */
+
+#ifndef HILL_CLIMBING_H_
+#define HILL_CLIMBING_H_
+#include <vector>
+#include <string>
+
+class Board
+{
+public:
+    int empty_spots = 0;
+    std::vector<std::string> state_vector;
+	std::vector<std::string> initial_state;
+	std::vector<int> sudoko_board;
+    Board(){};
+    void readFile(std::string filename);
+    void createStateVector()
+    {
+        char character('a');
+        for (int i = 0; i < initial_state.size(); i++)
+        {
+            if (initial_state[i].compare("*") == 0)
+            {
+                initial_state[i] = std::string(1, char(character + empty_spots));
+                state_vector.push_back(std::string(1, char(character + empty_spots)));
+                empty_spots++;
+            }
+        }
+    }
+
+};
+
+
+
+#endif /* HILL_CLIMBING_H_ */
---
+++ TestHarness.cpp
@@ -0,0 +1,61 @@
+/*
+ * TestHarness.cpp
+ *
+ *  Created on: Nov 14, 2014
+ *      Author: n8
+ */
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include "Hill_climbing.h"
+
+using namespace std;
+
+/* Reads inital board and stores into a Board instance variable */
+void Board::readFile(string filename)
+{
+    string line;
+    ifstream file;
+    bool first_line = true;
+    file.open(filename);
+    if( file.is_open())
+    {
+        while (getline(file, line))
+        {
+            if (line.compare("4") != 0)
+            {
+                for (int i = 0; i < line.length(); i++)
+                {
+                    cout << line[i] << endl;  // Don't need this later
+                    initial_state.push_back(string(1, line[i]));
+                }
+                first_line = false;
+            }
+        }
+        file.close();
+    }
+    else
+    {
+        cout << "Unable to open file." << endl;
+        exit(1);
+    }
+}
+
+
+
+int main(int argc, char ** argv)
+{
+	string filename("sudoku_data.txt");
+    Board* board = new Board();
+    board->readFile(filename);
+    board->createStateVector();
+    cout << board->empty_spots << endl;
+    
+    for (int i = 0; i < board->initial_state.size(); i++)
+    {
+        cout << board->initial_state[i] << endl;
+    }
+	return 0;
+}
